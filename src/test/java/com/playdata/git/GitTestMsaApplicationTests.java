bd917d057e366749b5872cfa1eb79536aea1af46
seanlee0923 <leesinue0923@gmail.com>
2023-10-23 10:21:29 +0900

    커밋

---
+++ src/test/java/com/playdata/git/GitTestMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestMsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
